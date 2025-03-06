c9260d8e6801c763e2c2f4174b002ed647e31331
Alexandre Demers <alexandre.f.demers@gmail.com>
2025-03-06 00:40:36 -0500

    drm/amdgpu: fix warning and errors caused by duplicated  defines in sid.h
    Let's finish the cleanup in sid.h to calm down things after wiring it into
    dce_v6_0.c.
    
    This is a bigger cleanup.
    Many defines found under sid.h have already been properly moved
    into the different "_d.h" and "_sh_mask.h", so they should have been
    already removed from sid.h and properly linked in where needed.
    
    Signed-off-by: Alexandre Demers <alexandre.f.demers@gmail.com>

--- drivers/gpu/drm/amd/amdgpu/dce_v6_0.c
+++ drivers/gpu/drm/amd/amdgpu/dce_v6_0.c
@@ -32,7 +32,6 @@
 #include "amdgpu.h"
 #include "amdgpu_pm.h"
 #include "amdgpu_i2c.h"
-#include "sid.h"
 #include "atom.h"
 #include "amdgpu_atombios.h"
 #include "atombios_crtc.h"
@@ -41,18 +40,25 @@
 #include "amdgpu_connectors.h"
 #include "amdgpu_display.h"
 
+#include "dce_v6_0.h"
+#include "sid.h"
+
 #include "bif/bif_3_0_d.h"
 #include "bif/bif_3_0_sh_mask.h"
+
 #include "oss/oss_1_0_d.h"
 #include "oss/oss_1_0_sh_mask.h"
+
 #include "gca/gfx_6_0_d.h"
 #include "gca/gfx_6_0_sh_mask.h"
+#include "gca/gfx_7_2_enum.h"
+
 #include "gmc/gmc_6_0_d.h"
 #include "gmc/gmc_6_0_sh_mask.h"
+
 #include "dce/dce_6_0_d.h"
 #include "dce/dce_6_0_sh_mask.h"
-#include "gca/gfx_7_2_enum.h"
-#include "dce_v6_0.h"
+
 #include "si_enums.h"
 
 static void dce_v6_0_set_display_funcs(struct amdgpu_device *adev);
